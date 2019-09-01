ceabb4267fe27a057a69946e29ae9eaa4aff2358
e <1273639096@qq.com>
2019-09-01 15:46:28 +0800

    第一次提交

---
+++ pages/taps.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="taps">
+      <span
+        v-for="(item, index) in ['新闻','娱乐','体育']"
+        :key=" index "
+        :class="{ active:cuttent === index }"
+        @click="changeTap(index)"
+      >{{ item }}</span>
+    </div>
+    <div v-show="cuttent===0">
+      <ul>
+        <li>新闻</li>
+        <li>新闻</li>
+        <li>新闻</li>
+      </ul>
+    </div>
+    <div v-show="cuttent===1">
+      <ul>
+        <li>娱乐</li>
+        <li>娱乐</li>
+        <li>娱乐</li>
+      </ul>
+    </div>
+    <div v-show="cuttent===2">
+      <ul>
+        <li>体育</li>
+        <li>体育</li>
+        <li>体育</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cuttent: 0
+    }
+  },
+  methods: {
+    changeTap (index) {
+      this.cuttent = index
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.taps{
+    padding-left:  50px;
+    span {
+        margin-left: 10px;
+        cursor: pointer;
+    }
+}
+.active {
+    color: hotpink;
+}
+</style>
