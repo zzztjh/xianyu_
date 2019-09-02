5a4ab2f584c0c24e614c02dc509207849fd63f67
e <1273639096@qq.com>
2019-09-02 15:54:23 +0800

    完成首页

---
+++ components/header.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="header">
+    <el-row type="flex" justify="space-between" class="main">
+      <div class="logo">
+        <nuxt-link to="/">
+          <img src="http://157.122.54.189:9093/images/logo.jpg" alt>
+        </nuxt-link>
+      </div>
+      <el-row class="nav" type="flex">
+        <nuxt-link to="/">
+          首页
+        </nuxt-link>
+        <nuxt-link to="/post">
+          旅游攻略
+        </nuxt-link>
+        <nuxt-link to="/hotel">
+          酒店
+        </nuxt-link>
+        <nuxt-link to="/air">
+          国内机票
+        </nuxt-link>
+      </el-row>
+      <el-row type="flex" class="login">
+        <nuxt-link to="user/login">
+          登陆 / 注册
+        </nuxt-link>
+      </el-row>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+.header {
+  height: 60px;
+  line-height: 60px;
+  box-sizing: border-box;
+  border-bottom: 1px solid #ddd;
+  box-shadow: 0 3px 0 #f5f5f5;
+  .main {
+    width: 1000px;
+    margin: 0 auto;
+  }
+}
+.nav {
+  flex: 1;
+  margin: 0 20px;
+  a {
+    display: block;
+    height: 60px;
+    line-height: 60px;
+    padding: 0 20px;
+    box-sizing: border-box;
+    &:hover {
+      border-bottom: 5px #409eff solid;
+      color: #409eff;
+    }
+  }
+  .nuxt-link-exact-active {
+    color: #fff;
+    background: #409eff;
+    &:hover {
+      background: #409eff;
+      color: #fff;
+    }
+  }
+}
+.logo img {
+  width: 156px;
+  height: 42px;
+  padding-top: 8px;
+}
+
+.login {
+  a {
+    font-size: 14px;
+    color: #666;
+    &:hover {
+      text-decoration: underline;
+      color: #409eff;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,220 @@
 <template>
   <div class="index">
-    首页
+    <!-- 轮播图 -->
+    <div class="container">
+      <el-carousel height="700px" arrow="always" class="banner" :interval="4000">
+        <el-carousel-item v-for="(item,index) in banners" :key="index">
+          <div
+            class="banner_img"
+            :style="`background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat`"
+          />
+        </el-carousel-item>
+      </el-carousel>
+      <!-- 搜索框 -->
+      <div class="banner-content">
+        <div class="search-bar">
+          <!-- tab栏 -->
+          <el-row
+            type="flex"
+            class="search-tab"
+          >
+            <span
+              v-for="(item, index) in options"
+              :key="index"
+              :class="{ active: index===current}"
+              @click="hanldIndex(index)"
+            >
+              <i>{{ item.text }}</i>
+            </span>
+          </el-row>
+
+          <!-- 输入框 -->
+          <el-row
+            type="flex"
+            align="middle"
+            class="search-input"
+          >
+            <input :placeholder="options[current].placeHolder">
+            <i class="el-icon-search" />
+          </el-row>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      current: 0,
+      options: [
+        {
+          text: '攻略',
+          placeHolder: '搜索城市'
+        },
+        {
+          text: '酒店',
+          placeHolder: '请输入城市搜索酒店'
+        },
+        {
+          text: '机票',
+          placeHolder: ''
+        }
+      ],
+      // 轮播图图片
+      banners: []
+    }
+  },
+  mounted () {
+    // 图片请求
+    this.$axios({
+      url: '/scenics/banners'
+    }).then((res) => {
+      // console.dir(this.$axios)
+      // const { data } = res.data
+      this.banners = res.data.data
+    })
+  },
+  methods: {
+    hanldIndex (index) {
+      if (index === 2) {
+        this.$router.push('/air')
+      }
+      this.current = index
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
+}
+.banner,
+.banner_img {
+  width: 100%;
+  height: 100%;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
 
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
+
+  .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
+  }
+}
 </style>
